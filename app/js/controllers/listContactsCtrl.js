cc1fbe151ef36a8477e5452160e26a9c65666bcf
Elena <elena.15@hotmail.it>
2015-02-05 18:44:17 +0000

    User can delete a contact. Problem with $locationProvider and angular mocks for unit tests --> currently are not passing.

--- app/js/controllers/listContactsCtrl.js
+++ app/js/controllers/listContactsCtrl.js
@@ -1,9 +1,15 @@
-addressBookApp.controller('listContactsCtrl', ['$scope', '$http',
-  function ($scope, $http) {
-    $http.get('http://fast-gorge.herokuapp.com/contacts').success(function(data) {
+addressBookApp.controller('listContactsCtrl', ['$scope', '$http', '$route',
+  function ($scope, $http, $route) {
+    $http.get('http://fast-gorge.herokuapp.com/contacts').success(function(data){
       $scope.contacts = data;
     });
 
     $scope.orderAlphabetically = 'surname';
+
+    $scope.deleteContact = function(id){
+      $http.delete('http://fast-gorge.herokuapp.com/contacts/' + id).success(function(){
+        $route.reload()
+      });
+    };
   }
 ]);
--- e2e-tests/addressBookHomePageSpec.js
+++ e2e-tests/addressBookHomePageSpec.js
@@ -1,4 +1,4 @@
-describe('The homepage', function(){
+describe('The address book', function(){
 
   beforeEach(function(){
     browser.get('index.html');
@@ -16,18 +16,18 @@ describe('The homepage', function(){
 
   it('a user should be able to search for a contact', function(){
     element(by.className('searchField')).sendKeys('nadia')
-    expect(contactList.get(1).getText()).toEqual('NOSTRA NADIA No Street no@no.com 01222312')
+    expect(contactList.get(1).getText()).toEqual('NOSTRA NADIA No Street no@no.com 01222312 Delete')
   });
 
   it('should be presented with the list of contacts ordered alphabetically by surname', function(){
-    expect(contactList.get(1).getText()).toEqual('ARTISTE ALANARAY 123 world and a half alan@art.com 03838 118101')
-    expect(contactList.get(2).getText()).toEqual('BRUNDY SOLOMON 110 kray road solo@brundy.com 87311337801')
+    expect(contactList.get(1).getText()).toEqual('ARTISTE ALANARAY 123 world and a half alan@art.com 03838 118101 Delete')
+    expect(contactList.get(2).getText()).toEqual('BRUNDY SOLOMON 110 kray road solo@brundy.com 87311337801 Delete')
   });
 
   it('a user should be able to list the contacts alphabetically by name', function(){
     element(by.model('orderAlphabetically')).element(by.css('option[value="first_name"]')).click();
-    expect(contactList.get(2).getText()).toEqual('GINOLA DAVID Any perrro street ginola@david.com 1981098312')
-    expect(contactList.get(3).getText()).toEqual('FOLLY FREDDY 273 england fred@folly.com 938289239181')
+    expect(contactList.get(2).getText()).toEqual('GINOLA DAVID Any perrro street ginola@david.com 1981098312 Delete')
+    expect(contactList.get(3).getText()).toEqual('FOLLY FREDDY 273 england fred@folly.com 938289239181 Delete')
   });
 
   it('should have a button to add a new contact', function(){
@@ -35,9 +35,38 @@ describe('The homepage', function(){
     expect(element(by.className('addContact')).getText()).toEqual('Add a new contact');
   });
 
-  it('by clicking on the "add contact" i should be redirected to another page', function(){
+  it('by clicking on the "Add a new contact" i should be redirected to another page', function(){
     element(by.className('addContact')).click();
     expect(browser.getCurrentUrl()).toContain('/new')
+  });
+
+  it('after clicking to add a new contact I should be presented with a form', function(){
+    element(by.className('addContact')).click();
+    expect(element(by.tagName('h1')).getText()).toEqual('Add a new contact');
+    expect(element(by.tagName('form')).isPresent()).toBe(true);
+  });
+
+  it('should be able to add a contact by filling up the form', function(){
+    element(by.className('addContact')).click();
+    element(by.id('exampleInputSurname1')).sendKeys('Zzzz')
+    element(by.id('exampleInputName1')).sendKeys('Zzzz')
+    element(by.className('submit_newContact')).click()
+    element.all(by.repeater('contact in contacts')).then(function(rows) {
+      var lastRow = rows[rows.length -1]
+      expect(lastRow.getText()).toMatch('ZZZZ ZZZZ');
+    })
+  });
+
+  it('should have a delete button available for each contact', function(){
+    expect(element(by.className('deleteZzzzZzzz')).isPresent()).toBe(true);
+  });
+
+  it('clicking the delete button you remove the contact', function(){
+    element(by.className('deleteZzzzZzzz')).click()
+    element.all(by.repeater('contact in contacts')).then(function(rows) {
+      var lastRow = rows[rows.length -1]
+      expect(lastRow.getText()).not.toMatch('ZZZZ ZZZZ Delete');
+    })
   });
 
 })
